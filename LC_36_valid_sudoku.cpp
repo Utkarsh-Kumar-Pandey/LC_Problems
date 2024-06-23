6bab4433350bbd8355b6f623e0aeb751dc054501
Utkarsh-Kumar-Pandey <167773388+Utkarsh-Kumar-Pandey@users.noreply.github.com>
2024-06-23 21:20:32 +0530

    learned a new approach
    This question was solved by:-
    >Making a set containing string {For checking duplicacy in row ,column and subboxes[3x3] }
    > converting each non empty cell into a string 
    >storing the values of row column and boxes in string
     >  --Checking if any of three is already available in set  --

---
+++ LC_36_valid_sudoku.cpp
@@ -0,0 +1,31 @@
+//Implementation after watching Nick White's solution
+// https://www.youtube.com/watch?v=Pl7mMcBm2b8
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> valStore;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char currentVal = board[i][j];
+
+                // if the cell is filled
+                if (currentVal != '.') {
+
+                    // Store the values coming up
+                    string row = "row" + to_string(i) + currentVal;
+                    string col = "col" + to_string(j) + currentVal;
+                    string box = "subbox" + to_string(i / 3) + to_string(j / 3) + currentVal;
+
+                    if (!valStore.insert(row).second ||
+                        !valStore.insert(col).second ||
+                        !valStore.insert(box).second) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
