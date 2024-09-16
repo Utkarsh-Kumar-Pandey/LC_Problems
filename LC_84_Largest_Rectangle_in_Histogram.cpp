04546e9dcdb22d84f61043608510b476aa54ea15
Utkarsh Kumar Pandey <167773388+Utkarsh-Kumar-Pandey@users.noreply.github.com>
2024-09-16 12:46:56 +0530

    Update LC_84_Largest_Rectangle_in_Histogram.cpp

--- LC_84_Largest_Rectangle_in_Histogram.cpp
+++ LC_84_Largest_Rectangle_in_Histogram.cpp
@@ -1,33 +1,32 @@
-// MY FIRST APPROACH 
+// MY FIRST APPROACH
 // 29/99 TEST CASES
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        if(n==1){
+        int n = heights.size();
+        if (n == 1) {
             return heights[0];
         }
-        int j=0;
-        int mini=heights[0];
-        int maxi=INT_MIN;
-        for(int i=0;i<n-1;i++){
-            int mini=heights[i];
-            j=i+1;
-            mini=min(mini,heights[j]);
-            int ans=mini*2;
-            if(heights[i]==0){
-                mini=heights[j];
-                ans=mini;
+        int j = 0;
+        int mini = heights[0];
+        int maxi = INT_MIN;
+        for (int i = 0; i < n - 1; i++) {
+            int mini = heights[i];
+            j = i + 1;
+            mini = min(mini, heights[j]);
+            int ans = mini * 2;
+            if (heights[i] == 0) {
+                mini = heights[j];
+                ans = mini;
             }
-            if(heights[j]==0){
-                mini=heights[i];
-                ans=mini;
+            if (heights[j] == 0) {
+                mini = heights[i];
+                ans = mini;
             }
 
-            maxi=max(maxi,ans);
+            maxi = max(maxi, ans);
         }
         return maxi;
-        
     }
 };
 /*
@@ -51,58 +50,55 @@ Output
 Expected
 3*/
 
-
-//Uses a formula nse index -pse index -1 *the current height
-// Brute force 
+// Used a formula (nse - pse - 1) * height
+// Brute force
 // The formula used is heights[i]*(NSE[i]-PSE[i]-1)
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int maxi=0;
-        int n=heights.size();
-        vector<int>pse(n,-1);
-        vector<int>nse(n,-1);
-        stack<int>st;
+        int maxi = 0;
+        int n = heights.size();
+        vector<int> pse(n, -1);
+        vector<int> nse(n, -1);
+        stack<int> st;
 
         // Previous smaller element
-        for(int i=0;i<n;i++){
-            while(!st.empty() &&heights[st.top()]>=heights[i]){
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
                 st.pop();
             }
-            if(st.empty()){
-                pse[i]=-1;
-            }
-            else{
-                pse[i]=st.top();
+            if (st.empty()) {
+                pse[i] = -1;
+            } else {
+                pse[i] = st.top();
             }
             st.push(i);
         }
 
-        // Pop all the elements for next smaller element
-        while(!st.empty()){
+        // Popping all the elements to make the stack empty
+        while (!st.empty()) {
             st.pop();
         }
 
-        // Next smaller element
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() &&heights[st.top()]>=heights[i]){
+        //For Next smaller element
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
                 st.pop();
             }
-            if(st.empty()){
-                nse[i]=n;
-            }
-            else{
-                nse[i]=st.top();
+            if (st.empty()) {
+                nse[i] = n;
+            } else {
+                nse[i] = st.top();
             }
             st.push(i);
         }
 
         // Largest Area of Rectangle
-        for(int i=0;i<n;i++){
-            int length=nse[i]-pse[i]-1;
-            int area=heights[i]*length;
-            maxi=max(maxi,area);
+        for (int i = 0; i < n; i++) {
+            int length = nse[i] - pse[i] - 1;
+            int area = heights[i] * length;
+            maxi = max(maxi, area);
         }
-        return maxi;     
+        return maxi;
     }
 };
