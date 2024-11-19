5f16b6374aca4f48d859e85e3e77c4e37dc44405
Utkarsh Kumar Pandey <167773388+Utkarsh-Kumar-Pandey@users.noreply.github.com>
2024-11-19 23:57:15 +0530

    Create LC_153_Find_Minimum_in_Rotated.cpp

---
+++ LC_153_Find_Minimum_in_Rotated.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int mini = INT_MAX;
+        int low = 0;
+        int high = nums.size() - 1;
+        while (low <= high) {
+            int mid = (high + low) / 2;
+            if (nums[low] <= nums[mid]) {
+                mini = min(nums[low], mini);
+                low = mid + 1;
+            } else {
+                mini = min(nums[mid], mini);
+                high = mid - 1;
+            }
+        }
+        return mini;
+    }
+};
