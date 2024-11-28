3187bbbf6cf74c558c145370a9b653f1b3e45972
Utkarsh Kumar Pandey <167773388+Utkarsh-Kumar-Pandey@users.noreply.github.com>
2024-11-28 18:43:19 +0530

    Create LC_70_Climbing_Stairs.cpp

---
+++ LC_70_Climbing_Stairs.cpp
@@ -0,0 +1,21 @@
+/*
+This is done through the tabulation method 
+To solve DP 3 Thinggs must be kept in mind
+1. Try to represent any problems in terms of index
+2. Do all possible stuffs on that index, according to the problem statement
+3. Sum of all stuffs ->count all the ways , min(of all stuffs) -> Find min
+*/
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==0 || n==1){
+            return 1;
+        }
+        vector<int>dp(n+1);
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
