de2f2f23e80ab5f1a59ce0f52259eb1bafec34eb
Utkarsh Kumar Pandey <167773388+Utkarsh-Kumar-Pandey@users.noreply.github.com>
2024-12-03 16:43:31 +0530

    Create GFG_Geeks_Training.cpp

---
+++ GFG_Geeks_Training.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int func(int day, int last, vector<vector<int>>& arr, vector<vector<int>>& dp) {
+        if (day == 0) {
+            int maxi = 0;
+            for (int task = 0; task < 3; task++) {
+                if (task != last) { 
+                    maxi = max(maxi, arr[0][task]);
+                }
+            }
+            return maxi;
+        }
+        if (dp[day][last] != -1) {
+            return dp[day][last];
+        }
+        int maxi = 0;
+        for (int task = 0; task < 3; task++) {
+            if (task != last) {
+                int current = arr[day][task] + func(day - 1, task, arr, dp);
+                maxi = max(maxi, current);
+            }
+        }
+        return dp[day][last] = maxi;
+    }
+
+    int maximumPoints(vector<vector<int>>& arr, int n) {
+        vector<vector<int>> dp(n, vector<int>(4, -1));
+        return func(n - 1, 3, arr, dp);
+    }
+};
