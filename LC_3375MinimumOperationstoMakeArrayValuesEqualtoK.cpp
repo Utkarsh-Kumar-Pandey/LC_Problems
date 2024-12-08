bee2d7a0caeab6a93776057f0380af185fca228d
Utkarsh Kumar Pandey <167773388+Utkarsh-Kumar-Pandey@users.noreply.github.com>
2024-12-08 11:02:15 +0530

    Create LC_3375MinimumOperationstoMakeArrayValuesEqualtoK.cpp

---
+++ LC_3375MinimumOperationstoMakeArrayValuesEqualtoK.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        set<int> st;
+        for (auto it : nums) {
+            if(it<k){
+                return -1;
+            }
+            if(it>k){
+            st.insert(it);
+            }
+        }
+        return st.size();
+    }
+};
+
